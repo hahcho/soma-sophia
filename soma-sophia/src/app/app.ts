3263b229be0148be4acfd154091b5d5e9edbc757
Mario Daskalov <mariodaskalov@gmail.com>
2026-02-15 17:47:35 +0200

    Introduce a header component
    It will be a navigation like style for the app. Style WIP

--- soma-sophia/src/app/app.ts
+++ soma-sophia/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard'
+import { Header } from './header/header'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('soma-sophia');
+  protected readonly headerText = 'Morning Routine';
 }
