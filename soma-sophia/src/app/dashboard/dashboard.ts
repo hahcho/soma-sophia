2e00b043ac8af7cbaade68d39025ab39a6e7df06
Mario Daskalov <mariodaskalov@gmail.com>
2026-02-17 19:35:37 +0200

    Introduce dashboard component v1

--- soma-sophia/src/app/dashboard/dashboard.ts
+++ soma-sophia/src/app/dashboard/dashboard.ts
@@ -1,45 +1,135 @@
 import { signal, Component } from '@angular/core';
 
-const HARDCODED_ROUTINE = `# Warm Up
+type SimpleExercise = {
+    kind: 'simple';
+    name: string;
+}
 
-* Neck circles // check for better naming
-* Cat/cow pose
-* Wrist opposite palm circles
-* Wrist top circles
-* Ankle sitting
+type StaticExercise = {
+    kind: 'static';
+    name: 'string';
+    holdTime: number;
+}
 
-## Shoulder mobility
-* Foam roll scapula and triceps
-* Elevated Straight arm push down 60s - 120s // check for better naming
-* laying straight arm rows // check for better naming
+type DynamicExercise = {
+    kind: 'dynamic';
+    name: 'string';
+    repetitions: number;
+}
 
-## Hamstring/Pike
+type Exercise = SimpleExercise | StaticExercise | DynamicExercise;
 
-* Chair seated hip hinge
-* Downward facing dog
-* Standing pike to squat and back
-* Mckenzi push ups
-* Seated pike
+type ExerciseSet = {
+    kind: 'set',
+    exercise: Exercise;
+    repetitions?: number;
+    restTime?: number;
+}
 
-# Strength
+type ExerciseSuperSet = {
+    kind: 'superset',
+    exercises: Exercise[];
+    repetitions?: number;
+    restTime?: number;
+}
 
-* bend leg v sit on floor 3x[time]
-2 min rest
-* 3x
-    * push ups x10
-    * chest to bar pull ups x5
-    * compression work x12
-2 min rest
-* 3x
-    * reverse elevated plank x30sec
-    * ring holds x30sec`
+type Phase = {
+    name: string;
+    sets: (ExerciseSet | ExerciseSuperSet)[];
+}
+
+type Routine = {name: string; phases: Phase[];};
+
+const HARDCODED_ROUTINE_JSON = {
+    name: 'Morning Routine',
+    phases: [
+        {
+            name: 'Warm up',
+            sets: [
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'simple', name: 'Neck circles // check for better naming'},
+                        {kind: 'simple', name: 'Cat/cow pose'},
+                        {kind: 'simple', name: 'Wrist opposite palm circles'},
+                        {kind: 'simple', name: 'Wrist top circles'},
+                        {kind: 'simple', name: 'Ankle sitting'}
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Shoulder mobility',
+            sets: [
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'simple', name: 'Foam roll scapula and triceps'},
+                        {kind: 'simple', name: 'Elevated Straight arm push down 60s - 120s // check for better naming'},
+                        {kind: 'simple', name: 'laying straight arm rows // check for better naming'}
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Hamstring/Pike',
+            sets: [
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'simple', name: 'Chair seated hip hinge'},
+                        {kind: 'simple', name: 'Downward facing dog'},
+                        {kind: 'simple', name: 'Standing pike to squat and back'},
+                        {kind: 'simple', name: 'Mckenzi push ups'},
+                        {kind: 'simple', name: 'Seated pike'},
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Strength',
+            sets: [
+                {
+                    kind: 'set',
+                    exercise: {
+                        kind: 'static',
+                        name: 'bend leg v sit on floor',
+                        holdTime: 30,
+                    },
+                    repetitions: 3,
+                    restTime: 120,
+                },
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'dynamic', name: 'push ups', repetitions: 10},
+                        {kind: 'dynamic', name: 'chest to bar pull ups', repetitions: 5},
+                        {kind: 'dynamic', name: 'compression work', repetitions: 12}
+                    ],
+                    repetitions: 3,
+                    restTime: 120,
+                },
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'static', name: 'reverse elevated plank', holdTime: 30},
+                        {kind: 'static', name: 'ring holds', holdTime: 30}
+
+                    ],
+                    repetitions: 3,
+                    restTime: 120,
+                }
+            ]
+        }
+    ]
+};
 
 @Component({
   selector: 'ss-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.scss',
 })
 export class Dashboard {
-    protected readonly routine = signal(HARDCODED_ROUTINE);
+    protected readonly routine = signal(HARDCODED_ROUTINE_JSON as Routine);
 }
