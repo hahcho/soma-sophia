f01774d79f7f576d15f690097753b632fea6faa2
Mario Daskalov <mariodaskalov@gmail.com>
2026-02-21 13:15:00 +0200

    Extract service for routine retrival

---
+++ soma-sophia/src/app/routine.service.ts
@@ -0,0 +1,163 @@
+import {Injectable} from '@angular/core';
+
+export type SimpleExercise = {
+    kind: 'simple';
+    name: string;
+}
+
+export type StaticExercise = {
+    kind: 'static';
+    name: string;
+    holdTime: number;
+}
+
+export type DynamicExercise = {
+    kind: 'dynamic';
+    name: string;
+    repetitions: number;
+}
+
+export type Exercise = SimpleExercise | StaticExercise | DynamicExercise;
+
+export type ExerciseSet = {
+    kind: 'set',
+    exercise: Exercise;
+    repetitions?: number;
+    restTime?: number;
+}
+
+export type ExerciseSuperSet = {
+    kind: 'superset',
+    exercises: Exercise[];
+    repetitions?: number;
+    restTime?: number;
+}
+
+export type Phase = {
+    name: string;
+    sets: (ExerciseSet | ExerciseSuperSet)[];
+}
+
+export class Routine {
+    name: string;
+    phases: Phase[];
+
+    constructor({name, phases}: {name: string; phases: Phase[]}) {
+        this.name = name;
+        this.phases = phases;
+    }
+
+    approximateTime() {
+        const singleSetApproximateTime = 60;
+        let totalApproximateTime = 0;
+
+        for (let phase of this.phases) {
+            for (let set of phase.sets) {
+                const repetitions = set.repetitions || 0;
+                const restTime = set.restTime || 0;
+                const totalSetTime = (singleSetApproximateTime + restTime) * repetitions;
+                totalApproximateTime += totalSetTime;
+            }
+        }
+
+        return totalApproximateTime;
+    }
+
+    totalExercises() {
+        let totalExercises = 0;
+        for (let phase of this.phases) {
+            totalExercises += phase.sets.length;
+        }
+        return totalExercises;
+    }
+}
+
+const HARDCODED_ROUTINE: {name: string; phases: Phase[]} = {
+    name: 'Morning Routine',
+    phases: [
+        {
+            name: 'Warm up',
+            sets: [
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'simple', name: 'Neck circles // check for better naming'},
+                        {kind: 'simple', name: 'Cat/cow pose'},
+                        {kind: 'simple', name: 'Wrist opposite palm circles'},
+                        {kind: 'simple', name: 'Wrist top circles'},
+                        {kind: 'simple', name: 'Ankle sitting'}
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Shoulder mobility',
+            sets: [
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'simple', name: 'Foam roll scapula and triceps'},
+                        {kind: 'simple', name: 'Elevated Straight arm push down 60s - 120s // check for better naming'},
+                        {kind: 'simple', name: 'laying straight arm rows // check for better naming'}
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Hamstring/Pike',
+            sets: [
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'simple', name: 'Chair seated hip hinge'},
+                        {kind: 'simple', name: 'Downward facing dog'},
+                        {kind: 'simple', name: 'Standing pike to squat and back'},
+                        {kind: 'simple', name: 'Mckenzi push ups'},
+                        {kind: 'simple', name: 'Seated pike'},
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Strength',
+            sets: [
+                {
+                    kind: 'set',
+                    exercise: {
+                        kind: 'static',
+                        name: 'bend leg v sit on floor',
+                        holdTime: 30,
+                    },
+                    repetitions: 3,
+                    restTime: 90,
+                },
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'dynamic', name: 'push ups', repetitions: 10},
+                        {kind: 'dynamic', name: 'chest to bar pull ups', repetitions: 5},
+                        {kind: 'dynamic', name: 'compression work', repetitions: 12}
+                    ],
+                    repetitions: 3,
+                    restTime: 120,
+                },
+                {
+                    kind: 'superset',
+                    exercises: [
+                        {kind: 'static', name: 'reverse elevated plank', holdTime: 30},
+                        {kind: 'static', name: 'ring holds', holdTime: 30}
+                    ],
+                    repetitions: 3,
+                    restTime: 120,
+                }
+            ]
+        }
+    ]
+};
+
+@Injectable({providedIn: 'root'})
+export class RoutineService {
+    getRoutine(): Routine {
+        return new Routine(HARDCODED_ROUTINE);
+    }
+}
